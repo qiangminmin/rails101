ed2e0e10f7615916a66909b206f6be69b2b52407
kuangmin <qiangminmin520@gmail.com>
2017-03-26 21:48:34 +0800

    using scope to write order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,7 @@ class GroupsController < ApplicationController
   def index
       @groups = Group.all
     end
+    
     def show
       @group = Group.find(params[:id])
       @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
